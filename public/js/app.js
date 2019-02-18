c63705f8dc791aa8d558dc4c6a0354ee02f8603e
Dxnax <daniel.ruiz@cceo.com.mx>
2019-02-18 15:20:39 -0600

    Video 63 Laravel Usuarios vistas parte 2

--- public/js/app.js
+++ public/js/app.js
@@ -42103,6 +42103,15 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 //
 //
 //
+//
+//
+//
+//
+//
+//
+//
+//
+//
 
 /* harmony default export */ __webpack_exports__["default"] = ({
     data: function data() {
@@ -42118,6 +42127,7 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
             password: '',
             idrol: 0,
             arrayPersona: [],
+            arrayRol: [],
             modal: 0,
             tituloModal: '',
             tipoAccion: 0,
@@ -42173,6 +42183,16 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
                 var respuesta = response.data;
                 me.arrayPersona = respuesta.personas.data;
                 me.pagination = respuesta.pagination;
+            }).catch(function (error) {
+                console.log(error);
+            });
+        },
+        selectRol: function selectRol() {
+            var me = this;
+            var url = '/rol/selectRol';
+            axios.get(url).then(function (response) {
+                var respuesta = response.data;
+                me.arrayRol = respuesta.roles;
             }).catch(function (error) {
                 console.log(error);
             });
@@ -42240,18 +42260,20 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
             this.modal = 0;
             this.tituloModal = '';
             this.nombre = '';
-            this.tipo_documento = 'RUC';
+            this.tipo_documento = 'DNI';
             this.num_documento = '';
             this.direccion = '';
             this.telefono = '';
             this.email = '';
-            this.contacto = '';
-            this.telefono_contacto = '';
+            this.usuario = '';
+            this.password = '';
+            this.idrol = 0;
             this.errorPersona = 0;
         },
         abrirModal: function abrirModal(modelo, accion) {
             var data = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : [];
 
+            this.selectRol();
             switch (modelo) {
                 case "persona":
                     {
@@ -42259,15 +42281,16 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
                             case 'registrar':
                                 {
                                     this.modal = 1;
-                                    this.tituloModal = 'Registrar Proveedor';
+                                    this.tituloModal = 'Registrar Usuario';
                                     this.nombre = '';
-                                    this.tipo_documento = 'RUC';
+                                    this.tipo_documento = 'DNI';
                                     this.num_documento = '';
                                     this.direccion = '';
                                     this.telefono = '';
                                     this.email = '';
-                                    this.contacto = '';
-                                    this.telefono_contacto = '';
+                                    this.usuario = '';
+                                    this.password = '';
+                                    this.idrol = 0;
                                     this.tipoAccion = 1;
                                     break;
                                 }
@@ -42275,7 +42298,7 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
                                 {
                                     //console.log(data);
                                     this.modal = 1;
-                                    this.tituloModal = 'Actualizar Proveedor';
+                                    this.tituloModal = 'Actualizar Usuario';
                                     this.tipoAccion = 2;
                                     this.persona_id = data['id'];
                                     this.nombre = data['nombre'];
@@ -42284,8 +42307,9 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
                                     this.direccion = data['direccion'];
                                     this.telefono = data['telefono'];
                                     this.email = data['email'];
-                                    this.contacto = data['contacto'];
-                                    this.telefono_contacto = data['telefono_contacto'];
+                                    this.usuario = data['usuario'];
+                                    this.password = data['password'];
+                                    this.idrol = data['idrol'];
                                     break;
                                 }
                         }
@@ -42877,7 +42901,66 @@ var render = function() {
                           staticClass: "col-md-3 form-control-label",
                           attrs: { for: "text-input" }
                         },
-                        [_vm._v("Contacto")]
+                        [_vm._v("Rol (*)")]
+                      ),
+                      _vm._v(" "),
+                      _c("div", { staticClass: "col-md-9" }, [
+                        _c(
+                          "select",
+                          {
+                            directives: [
+                              {
+                                name: "model",
+                                rawName: "v-model",
+                                value: _vm.idrol,
+                                expression: "idrol"
+                              }
+                            ],
+                            staticClass: "form-control",
+                            on: {
+                              change: function($event) {
+                                var $$selectedVal = Array.prototype.filter
+                                  .call($event.target.options, function(o) {
+                                    return o.selected
+                                  })
+                                  .map(function(o) {
+                                    var val = "_value" in o ? o._value : o.value
+                                    return val
+                                  })
+                                _vm.idrol = $event.target.multiple
+                                  ? $$selectedVal
+                                  : $$selectedVal[0]
+                              }
+                            }
+                          },
+                          [
+                            _c("option", { attrs: { value: "0" } }, [
+                              _vm._v("Seleccione un rol")
+                            ]),
+                            _vm._v(" "),
+                            _vm._l(_vm.arrayRol, function(rol) {
+                              return _c("option", {
+                                key: rol.id,
+                                domProps: {
+                                  value: rol.id,
+                                  textContent: _vm._s(rol.nombre)
+                                }
+                              })
+                            })
+                          ],
+                          2
+                        )
+                      ])
+                    ]),
+                    _vm._v(" "),
+                    _c("div", { staticClass: "form-group row" }, [
+                      _c(
+                        "label",
+                        {
+                          staticClass: "col-md-3 form-control-label",
+                          attrs: { for: "text-input" }
+                        },
+                        [_vm._v("Usuario (*)")]
                       ),
                       _vm._v(" "),
                       _c("div", { staticClass: "col-md-9" }, [
@@ -42886,19 +42969,22 @@ var render = function() {
                             {
                               name: "model",
                               rawName: "v-model",
-                              value: _vm.contacto,
-                              expression: "contacto"
+                              value: _vm.usuario,
+                              expression: "usuario"
                             }
                           ],
                           staticClass: "form-control",
-                          attrs: { type: "text", placeholder: "Contacto" },
-                          domProps: { value: _vm.contacto },
+                          attrs: {
+                            type: "text",
+                            placeholder: "Nombre de usuario"
+                          },
+                          domProps: { value: _vm.usuario },
                           on: {
                             input: function($event) {
                               if ($event.target.composing) {
                                 return
                               }
-                              _vm.contacto = $event.target.value
+                              _vm.usuario = $event.target.value
                             }
                           }
                         })
@@ -42912,7 +42998,7 @@ var render = function() {
                           staticClass: "col-md-3 form-control-label",
                           attrs: { for: "text-input" }
                         },
-                        [_vm._v("Teléfono de contacto")]
+                        [_vm._v("Password (*)")]
                       ),
                       _vm._v(" "),
                       _c("div", { staticClass: "col-md-9" }, [
@@ -42921,22 +43007,22 @@ var render = function() {
                             {
                               name: "model",
                               rawName: "v-model",
-                              value: _vm.telefono_contacto,
-                              expression: "telefono_contacto"
+                              value: _vm.password,
+                              expression: "password"
                             }
                           ],
                           staticClass: "form-control",
                           attrs: {
-                            type: "text",
-                            placeholder: "Telefono del contacto"
+                            type: "password",
+                            placeholder: "Password de acceso"
                           },
-                          domProps: { value: _vm.telefono_contacto },
+                          domProps: { value: _vm.password },
                           on: {
                             input: function($event) {
                               if ($event.target.composing) {
                                 return
                               }
-                              _vm.telefono_contacto = $event.target.value
+                              _vm.password = $event.target.value
                             }
                           }
                         })
--- resources/assets/js/components/User.vue
+++ resources/assets/js/components/User.vue
@@ -132,15 +132,24 @@
                             </div>
                         </div>
                         <div class="form-group row">
-                            <label class="col-md-3 form-control-label" for="text-input">Contacto</label>
+                            <label class="col-md-3 form-control-label" for="text-input">Rol (*)</label>
                             <div class="col-md-9">
-                                <input type="text" v-model="contacto" class="form-control" placeholder="Contacto">                               
+                                <select class="form-control" v-model="idrol">
+                                    <option value="0">Seleccione un rol</option>
+                                    <option v-for="rol in arrayRol" :key="rol.id" :value="rol.id" v-text="rol.nombre"></option>
+                                </select>
                             </div>
                         </div>
                         <div class="form-group row">
-                            <label class="col-md-3 form-control-label" for="text-input">Teléfono de contacto</label>
+                            <label class="col-md-3 form-control-label" for="text-input">Usuario (*)</label>
                             <div class="col-md-9">
-                                <input type="text" v-model="telefono_contacto" class="form-control" placeholder="Telefono del contacto">                               
+                                <input type="text" v-model="usuario" class="form-control" placeholder="Nombre de usuario">                               
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label class="col-md-3 form-control-label" for="text-input">Password (*)</label>
+                            <div class="col-md-9">
+                                <input type="password" v-model="password" class="form-control" placeholder="Password de acceso">                               
                             </div>
                         </div>                   
                         <div v-show="errorPersona" class="form-group row div-error">
@@ -181,6 +190,7 @@
                 password: '',
                 idrol: 0,
                 arrayPersona : [],
+                arrayRol : [],
                 modal : 0,
                 tituloModal : '',
                 tipoAccion : 0,
@@ -235,6 +245,18 @@
                     var respuesta = response.data;
                     me.arrayPersona = respuesta.personas.data;
                     me.pagination = respuesta.pagination;
+                })
+                .catch(function (error){
+                    console.log(error);
+                });
+            },
+            selectRol(){
+                let me=this;
+                var url = '/rol/selectRol';
+                axios.get(url).then(function (response) {
+                    var respuesta = response.data;
+                    me.arrayRol = respuesta.roles;
+                    
                 })
                 .catch(function (error){
                     console.log(error);
@@ -303,16 +325,18 @@
                 this.modal = 0;
                 this.tituloModal = '';
                 this.nombre = '';
-                this.tipo_documento = 'RUC';
+                this.tipo_documento = 'DNI';
                 this.num_documento = '';
                 this.direccion = '';
                 this.telefono = '';
                 this.email = '';
-                this.contacto = '';
-                this.telefono_contacto = '';
+                this.usuario = '';
+                this.password = '';
+                this.idrol = 0;
                 this.errorPersona = 0;
             },
             abrirModal(modelo, accion, data = []){
+                this.selectRol();
                 switch (modelo){
                     case "persona":
                     {
@@ -320,15 +344,16 @@
                             case 'registrar':
                             {
                                 this.modal = 1;
-                                this.tituloModal = 'Registrar Proveedor'
+                                this.tituloModal = 'Registrar Usuario'
                                 this.nombre = '';
-                                this.tipo_documento = 'RUC';
+                                this.tipo_documento = 'DNI';
                                 this.num_documento = '';
                                 this.direccion = '';
                                 this.telefono = '';
                                 this.email = '';
-                                this.contacto = '';
-                                this.telefono_contacto = '';
+                                this.usuario = '';
+                                this.password = '';
+                                this.idrol = 0;
                                 this.tipoAccion = 1;
                                 break;
                             }
@@ -336,7 +361,7 @@
                             {
                                 //console.log(data);
                                 this.modal = 1;
-                                this.tituloModal = 'Actualizar Proveedor'
+                                this.tituloModal = 'Actualizar Usuario'
                                 this.tipoAccion = 2;
                                 this.persona_id = data['id'];
                                 this.nombre = data['nombre'];
@@ -345,8 +370,9 @@
                                 this.direccion = data['direccion'];
                                 this.telefono = data['telefono'];
                                 this.email = data['email'];
-                                this.contacto = data['contacto'];
-                                this.telefono_contacto = data['telefono_contacto'];
+                                this.usuario = data['usuario'];
+                                this.password = data['password'];
+                                this.idrol = data['idrol']
                                 break;
                             }
                         }
