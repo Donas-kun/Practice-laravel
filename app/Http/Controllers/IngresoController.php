f5b4651155b67995db14680310d21343b55adaca
Dxnax <daniel.ruiz@cceo.com.mx>
2019-02-19 17:06:12 -0600

    Video 75 Laravel

---
+++ app/Http/Controllers/IngresoController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\DB;
+use App\Ingreso;
+use App\DetalleIngreso;
+
+class IngresoController extends Controller
+{
+    public function index(Request $request)
+    {
+        if (!$request->ajax()) return redirect('/');
+
+        $buscar = $request->buscar;
+        $criterio = $request->criterio;
+
+        if ($buscar == ''){
+            $ingresos = Ingreso::join('personas', 'ingresos.idproveedor', '=', 'personas.id')
+            ->join('users', 'ingresos.idusuario', '=', 'users.id')
+            ->select('ingresos.id', 'ingresos.tipo_comprobante', 'ingresos.serie_comprobante', 'ingresos.num_comprobante','ingresos.fecha_hora', 'ingresos.impuesto',
+            'ingresos.total', 'ingresos.estado', 'personas.nombre', 'users.usuario')   
+            ->orderBy('ingresos.id', 'desc')->paginate(3);
+        }
+        else{
+            $personas = Ingreso::join('personas', 'ingresos.idproveedor', '=', 'personas.id')
+            ->join('users', 'ingresos.idusuario', '=', 'users.id')
+            ->select('ingresos.id', 'ingresos.tipo_comprobante', 'ingresos.serie_comprobante', 'ingresos.num_comprobante','ingresos.fecha_hora', 'ingresos.impuesto',
+            'ingresos.total', 'ingresos.estado', 'personas.nombre', 'users.usuario')   
+            ->whre('ingresos.'.$criterio, 'like', '%'. $buscar. '%')->orderBy('ingresos.id', 'desc')->paginate(3);
+        }
+        return[
+            'pagination' => [
+                'total'         =>  $ingresos->total(),
+                'current_page'  =>  $ingresos->currentPage(),
+                'per_page'      =>  $ingresos->perPage(),
+                'last_page'     =>  $ingresos->lastPage(),
+                'from'          =>  $ingresos->firstItem(),
+                'to'            =>  $ingresos->lastItem(),
+            ],
+            'ingresos' => $ingresos
+        ];
+    }
+
+    public function store(Request $request)
+    {
+        if (!$request->ajax()) return redirect('/');
+
+        try {
+            DB::beginTransaction();
+
+            $mytime= Carbon::now('America/Monterrey');
+
+            $ingreso = new Ingreso();
+            $ingreso->idproveedor = $request->idproveedor;
+            $ingreso->idusuario = \Auth::user()->id;
+            $ingreso->tipo_comprobante = $request->tipo_comprobante;
+            $ingreso->serie_comprobante = $request->serie_comprobante;
+            $ingreso->num_comprobante = $request->num_comprobante;
+            $ingreso->fecha_hora = $mytime->toDateString();
+            $ingreso->impuesto = $request->impuesto;
+            $ingreso->total = $request->total;
+            $ingreso->estado = 'Registrado';
+            $ingreso->save();
+
+            $detalles = $request->data;//Array de detalles
+
+            foreach($detalles as $ep=>$det){
+                $detalle = new DetalleIngreso();
+                $detalle->idingreso = $ingreso->id;
+                $detalle->idarticulo = $det['idarticulo'];
+                $detalle->cantidad = $det['cantidad'];
+                $detalle->precio = $det['precio'];
+                $detalle->save();
+            }
+
+            DB::commit();
+        } catch (Exception $e) {
+            DB::rollback();
+        }
+
+       
+    }
+
+    public function desactivar(Request $request)
+    {
+        if (!$request->ajax()) return redirect('/');
+        $ingreso = Ingreso::findOrFail($request->id);
+        $ingreso->estado = 'Anulado';
+        $ingreso->save();
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -39,6 +39,10 @@ Route::group(['middleware' => ['auth']], function () {
         Route::get('/proveedor', 'ProveedorController@index');
         Route::post('/proveedor/registrar', 'ProveedorController@store');
         Route::put('/proveedor/actualizar', 'ProveedorController@update');
+
+        Route::get('/ingreso', 'IngresoController@index');
+        Route::post('/ingreso/registrar', 'IngreoController@store');
+        Route::put('/ingreso/desactivar', 'IngreoController@desactivar');
     });//Route::group(['middleware' => ['Almacenero']],function () {});
     
     Route::group(['middleware' => ['Vendedor']],function () {
@@ -64,6 +68,10 @@ Route::group(['middleware' => ['auth']], function () {
         Route::get('/proveedor', 'ProveedorController@index');
         Route::post('/proveedor/registrar', 'ProveedorController@store');
         Route::put('/proveedor/actualizar', 'ProveedorController@update');
+
+        Route::get('/ingreso', 'IngresoController@index');
+        Route::post('/ingreso/registrar', 'IngreoController@store');
+        Route::put('/ingreso/desactivar', 'IngreoController@desactivar');
 
         Route::get('/cliente', 'ClienteController@index');
         Route::post('/cliente/registrar', 'ClienteController@store');
